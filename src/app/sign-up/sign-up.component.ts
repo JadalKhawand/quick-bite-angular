3e26ac03ffdd6554a0d827f2cfbd752aa00ebb92
Jad alKhawand <jkh42005@gmail.com>
2024-03-05 21:48:55 +0200

    updates: ' :

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -5,18 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-password = ''
-passwordRepeat = ''
-onChangePassword(event: Event){
-  this.password=(event.target as HTMLInputElement).value
-}
-onChangePasswordRepeat(event: Event){
-  this.passwordRepeat=(event.target as HTMLInputElement).value
-}
-isDisabled(){
-  return this.password? (this.password !== this.passwordRepeat) : true
-}
+  password = '';
+  passwordRepeat = '';
+  onChangePassword(event: Event) {
+    this.password = (event.target as HTMLInputElement).value;
+  }
+  onChangePasswordRepeat(event: Event) {
+    this.passwordRepeat = (event.target as HTMLInputElement).value;
+  }
+  isDisabled() {
+    return this.password ? this.password !== this.passwordRepeat : true;
+  }
 }
